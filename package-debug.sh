509ed691a58ecd6c98b2b693e3590e0b22d67feb
H2Sxxa <h2sxxa0w0@gmail.com>
2025-12-13 11:18:04 +0800

    更新构建和打包脚本，将输出目录和名称统一为CCZUniSwift，并添加将包推送到CCZUniSwift仓库的步骤

--- package-debug.sh
+++ package-debug.sh
@@ -1,7 +1,7 @@
 swift-bridge-cli create-package \
   --bridges-dir ./generated \
-  --out-dir CCZUNISwiftBridge \
+  --out-dir CCZUniSwift \
   --ios target/aarch64-apple-ios/debug/libcczuni_swift_bridge.a \
   --simulator target/universal-ios/debug/libcczuni_swift_bridge.a \
   --macos target/universal-macos/debug/libcczuni_swift_bridge.a \
-  --name CCZUNISwiftBridge
+  --name CCZUniSwift
